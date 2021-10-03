6c2f0d35184fde513ee10ca1fd7e7a97e999b6f4
Darshan Ponikar <itsponikar@gmail.com>
2021-10-03 14:17:48 +0530

    working with hero

--- src/components/hero/header/header.tsx
+++ src/components/hero/header/header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Search } from "../../search/search";
 import { Hero } from "../hero";
 
 export const Header = () => {
   return (
     <>
-      <nav className="px-4 fixed py-2 w-full items-center flex justify-between">
+      <nav className="px-4 fixed sm:py-2 py-3 w-full items-center flex justify-between">
         <section className="sm:w-7/12 w-10/12 items-center justify-start flex">
           <img
             src="/imgs/logo.svg"
@@ -15,18 +16,28 @@ export const Header = () => {
             Pexels
           </h1>
 
-          <input
-            type="text"
-            placeholder="Search for free Photos"
-            className="text-black text-lg placeholder-gray-500  ml-5 flex-grow font-light rounded-md px-4 py-2"
-          />
+          <Search className="sm:ml-5 ml-3 flex-grow " />
         </section>
         <ul className="sm:flex hidden items-center justify-center w-4/12">
-          <li className="text-base ml-10 text-white">Explore</li>
-          <li className="text-base ml-10 text-white">License</li>
-          <li className="text-base ml-10 text-white">Bell</li>
-          <li className="text-base ml-10 text-white">User</li>
-          <li className="text-base ml-10 text-white">
+          <li className="text-base ml-7 text-white">Explore</li>
+          <li className="text-base ml-7 text-white">License</li>
+          <li className="text-base ml-7 text-white">
+            <img
+              src="/imgs/bell.svg"
+              alt="Notifications from Pexels"
+              className="w-6"
+            />
+          </li>
+          <li className="text-base ml-7 text-white item-center flex">
+            <img
+              src="https://www.gravatar.com/avatar/cc579483542fb5af21184bad8b23f9f8?s=60&d=mm"
+              alt="User's Profile Picture"
+              className="w-6 rounded-full border-2 border-white"
+            />
+
+            <img className="ml-2" src="/imgs/more-arrow.svg" />
+          </li>
+          <li className="text-base ml-7 text-white">
             <button className="py-2 px-7 font-semibold bg-primary rounded-sm">
               Login
             </button>
--- src/components/images/images-area.tsx
+++ src/components/images/images-area.tsx
@@ -6,7 +6,7 @@ import { ImageCollections } from "./images-collection";
 export const ImageArea = () => {
   const { isLoading } = useImages();
   return (
-    <section className="grid sm:w-10/12 w-11/12 mx-auto sm:gap-5 gap-1 grid-cols-3 border">
+    <section className="grid sm:w-10/12 w-11/12 mx-auto sm:gap-5 gap-1 grid-cols-3">
       {isLoading ? <ImageLoading /> : <ImageCollections />}
     </section>
   );
